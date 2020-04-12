ecd92cb9039e5fdc0307efe6f34ef10f14e6c229
AmarnathN <vayuteja@gmail.com>
2020-04-12 07:08:07 +0530

    Updated the Favourites fall back text

--- alacarte/App/screens/FavouritesScreen.js
+++ alacarte/App/screens/FavouritesScreen.js
@@ -4,10 +4,19 @@ import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import MealList from "../components/MealList";
 import MyHeaderButton from "../components/MyHeaderButton";
+import { View, Text, StyleSheet } from "react-native";
+import MyText from "../components/MyText";
 
 const FavouritesScreen = (props) => {
   const favMeals = useSelector((state) => state.meals.favouriteMeals);
 
+  if (favMeals.length == 0 || !favMeals) {
+    return (
+      <View style={styles.container}>
+        <MyText>No favourite Meals as of now, start adding some !!</MyText>
+      </View>
+    );
+  }
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
 FavouritesScreen.navigationOptions = (navigationData) => {
@@ -25,4 +34,13 @@ FavouritesScreen.navigationOptions = (navigationData) => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+});
 export default FavouritesScreen;
